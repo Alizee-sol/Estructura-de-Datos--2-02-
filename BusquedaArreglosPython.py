44bc243fd27b8d5f5c67b8bc8531793ae27188e0
Alizee <alizeesola.america@gmail.com>
2026-09-03 14:07:17 -0700

    Añadí busqueda secuencial

--- BusquedaArreglosPython.py
+++ BusquedaArreglosPython.py
@@ -1,4 +1,17 @@
 #Busqueda secuencial ***********************************************************************************
-def findEle()
+def findEle(arreglo, s, elemento):
+    for indice in range(s):
+        if (arreglo[indice] == elemento):
+            return indice
+    return -1
+if __name__ == '__main__':
+    arreglo=[12,34,10,6,40,89,98,57,19,69]
+    elemento=19
+    s = len(arreglo)
+    indice = findEle(arreglo, s, elemento)
+    if indice != -1:
+        print("El elemento se encuentra en la posicion: " + str(indice +1))
+    else:
+        print("No se encuentra el elemento")
 
 
