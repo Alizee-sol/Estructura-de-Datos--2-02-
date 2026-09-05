94955d400448a47a0959d384b1a35c8a9db2b8c5
Alizee <alizeesola.america@gmail.com>
2026-09-05 14:33:33 -0700

    Uso la funcion .remove solo con el indice 0,  hago los cambios debidos

---
+++ Java/eliminacionini_inicial.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class eliminacionini_inicial {
+   public static void main(String[] args) 
+    {
+        //Declaracion de array que pueda ser cambiado (dinamico)
+        ArrayList<Integer> eliminacionini = new ArrayList<>(Arrays.asList(11, 21, 31, 41, 51, 61));
+
+        System.out.print("Antes de la eliminacionini al inicio, el array es: ");
+
+        //variable que guarda el largo del array
+        int largo = eliminacionini.size();
+
+        //For para recorrer el array (sin eliminar nada)
+        for (int indice = 0; indice < largo; indice++) {
+            System.out.print(eliminacionini.get(indice) + " ");
+        }
+        System.out.println();
+        
+        //Función para eliminar un elemento del indice 0
+        eliminacionini.remove(0);
+        
+        //Recorre el arreglo final, ya con el elemento de menos
+        System.out.print("Después de la eliminacion al inicio, el array es: ");
+        for (int indice = 0; indice < largo; indice++) {
+            System.out.print(eliminacionini.get(indice) + " ");
+        }
+        System.out.println();
+    } 
+}
