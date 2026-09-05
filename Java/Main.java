beec6348afcb4a53b8c6e3455ded5be1b618eee5
Alizee <alizeesola.america@gmail.com>
2026-09-05 13:39:54 -0700

    Agrego comentarios explicativos

--- Java/Main.java
+++ Java/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        //Declaración del array estático
         int[] recorridoin = {40, 50, 60, 70, 80, 90};
+
         System.out.print("Recorrido inverso del array es: ");
-        int largo = recorridoin.length;
         
+        //Variable que guarda el largo del array para saber cuantos elementos hay
+        int largo = recorridoin.length;                       
+        
+        //"For" para bajar el indice desde el último elemento del array
         for (int indice = largo - 1; indice >= 0; indice--) {
             System.out.print(recorridoin[indice] + " ");
         }
