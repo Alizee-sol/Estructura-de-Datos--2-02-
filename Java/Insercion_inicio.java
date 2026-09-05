3a0da6d2d9a05f8c6b74087efc1b67ea6def3e8b
Alizee <alizeesola.america@gmail.com>
2026-09-05 14:01:27 -0700

    Uso función .add y recorro arreglo final

--- Java/Insercion_inicio.java
+++ Java/Insercion_inicio.java
@@ -7,7 +7,7 @@ public class Insercion_inicio
     {
         //Declaracion de array que pueda ser cambiado (dinamico)
         ArrayList<Integer> insercion = new ArrayList<>(Arrays.asList(11, 21, 31, 41, 51, 61));
-        int ele = 52;
+        int elemento = 52;
 
         System.out.print("Antes de la insercion, el array es: ");
 
@@ -20,5 +20,14 @@ public class Insercion_inicio
         }
         System.out.println();
         
+        //Función para agregar un elemento en el indice 0, y elimina el del ultimo indice
+        insercion.add(0, elemento);
+        
+        //Recorre el arreglo final, ya con el elemento nuevo
+        System.out.print("Después de la eliminacion, el array es: ");
+        for (int indice = 0; indice < largo; indice++) {
+            System.out.print(insercion.get(indice) + " ");
+        }
+        System.out.println();
     }
 }
