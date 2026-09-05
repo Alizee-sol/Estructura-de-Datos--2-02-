b2ba3095e8864b91a7fde07bbcc4747676e13bcb
Alizee <alizeesola.america@gmail.com>
2026-09-05 14:13:19 -0700

    Eliminé el indice de la funcion, he hice los cambios visuales

--- Java/Insercion_final.java
+++ Java/Insercion_final.java
@@ -5,27 +5,28 @@ public class Insercion_final {
     public static void main(String[] args) 
     {
         //Declaracion de array que pueda ser cambiado (dinamico)
-        ArrayList<Integer> insercion = new ArrayList<>(Arrays.asList(11, 21, 31, 41, 51, 61));
+        ArrayList<Integer> insercionfin = new ArrayList<>(Arrays.asList(11, 21, 31, 41, 51, 61));
         int elemento = 52;
 
-        System.out.print("Antes de la insercion, el array es: ");
+        System.out.print("Antes de la insercion al final, el array es: ");
 
         //variable que guarda el largo del array
-        int largo = insercion.size();
+        int largo = insercionfin.size();
 
         //For para recorrer el array (sin insertar nada)
         for (int indice = 0; indice < largo; indice++) {
-            System.out.print(insercion.get(indice) + " ");
+            System.out.print(insercionfin.get(indice) + " ");
         }
         System.out.println();
         
-        //Función para agregar un elemento en el indice 0, y elimina el del ultimo indice
-        insercion.add(0, elemento);
+        //Función para agregar un elemento
+        //Sin especificar el indice, asume que es al final del array
+        insercionfin.add(elemento);
         
         //Recorre el arreglo final, ya con el elemento nuevo
-        System.out.print("Después de la eliminacion, el array es: ");
+        System.out.print("Después de la insercion, el array es: ");
         for (int indice = 0; indice < largo; indice++) {
-            System.out.print(insercion.get(indice) + " ");
+            System.out.print(insercionfin.get(indice) + " ");
         }
         System.out.println();
     }
