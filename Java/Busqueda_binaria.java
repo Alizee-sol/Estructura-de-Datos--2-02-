45600bcc967354830b0f9e67adbe83398ad8f585
Alizee <alizeesola.america@gmail.com>
2026-09-05 16:35:23 -0700

    Pegué contenido de Busqueda secuencial

--- Java/Busqueda_binaria.java
+++ Java/Busqueda_binaria.java
@@ -4,5 +4,17 @@ public class Busqueda_binaria {
         int[] arreglo = {12, 34, 10, 6, 40, 89, 98, 57, 19, 69};
         int elemento = 40;
         int largo = arreglo.length; //variable que guarda el largo del arreglo
+
+        //Envío el arreglo(contenido), el largo(cuantos elementos) y el elemento a buscar
+        int indice = findEle(arreglo, largo, elemento);
+        
+        if(indice != -1)
+        {
+            System.out.println("El elemento se encuentra en la posicion: " + (indice + 1));
+        } 
+        else 
+        {
+            System.out.println("El elemento no está en el conjunto");
+        }
     }
 }
