918f9f8f020a653531cbf873159df6192f21b1fe
Alizee <alizeesola.america@gmail.com>
2026-09-05 13:48:02 -0700

    Importo librerias para crear arraylist

---
+++ Java/Insercion_inicio.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Insercion_inicio 
+{
+    public static void main(String[] args) 
+    {
+        //Declaracion de array que pueda ser cambiado (dinamico)
+        ArrayList<Integer> insercion = new ArrayList<>(Arrays.asList(11, 21, 31, 41, 51, 61));
+        int ele = 52;
+    }
+}
