a949c05a32793c1e4c819f91157c2ed3b4119fbe
Alizee <alizeesola.america@gmail.com>
2026-09-05 13:55:21 -0700

    For para recorrer el arreglo antes de insertar

--- Java/Insercion_inicio.java
+++ Java/Insercion_inicio.java
@@ -8,5 +8,17 @@ public class Insercion_inicio
         //Declaracion de array que pueda ser cambiado (dinamico)
         ArrayList<Integer> insercion = new ArrayList<>(Arrays.asList(11, 21, 31, 41, 51, 61));
         int ele = 52;
+
+        System.out.print("Antes de la insercion, el array es: ");
+
+        //variable que guarda el largo del array
+        int largo = insercion.size();
+
+        //For para recorrer el array (sin insertar nada)
+        for (int indice = 0; indice < largo; indice++) {
+            System.out.print(insercion.get(indice) + " ");
+        }
+        System.out.println();
+        
     }
 }
