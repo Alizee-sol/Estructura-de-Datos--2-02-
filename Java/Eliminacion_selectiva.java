68a5b11260b3a5d6e8f5cfd84664c5e4a48614b4
Alizee <alizeesola.america@gmail.com>
2026-09-05 14:40:33 -0700

    Utilicé la funcion .remove con el indice previamente declarado, e hice los cambios debidos

--- Java/Eliminacion_selectiva.java
+++ Java/Eliminacion_selectiva.java
@@ -5,26 +5,29 @@ public class Eliminacion_selectiva {
     public static void main(String[] args) 
     {
         //Declaracion de array que pueda ser cambiado (dinamico)
-        ArrayList<Integer> eliminacionini = new ArrayList<>(Arrays.asList(11, 21, 31, 41, 51, 61));
+        ArrayList<Integer> eliminacionselec = new ArrayList<>(Arrays.asList(11, 21, 31, 41, 51, 61));
 
-        System.out.print("Antes de la eliminacionini al inicio, el array es: ");
+        System.out.print("Antes de la eliminacion selectiva al inicio, el array es: ");
 
         //variable que guarda el largo del array
-        int largo = eliminacionini.size();
+        int largo = eliminacionselec.size();
 
         //For para recorrer el array (sin eliminar nada)
         for (int indice = 0; indice < largo; indice++) {
-            System.out.print(eliminacionini.get(indice) + " ");
+            System.out.print(eliminacionselec.get(indice) + " ");
         }
         System.out.println();
+
+        //indice que vamos a eliminar
+        int idx = 3;
         
-        //Función para eliminar un elemento del indice 0
-        eliminacionini.remove(0);
+        //Función para eliminar un elemento del indice idx
+        eliminacionselec.remove(idx - 1);
         
         //Recorre el arreglo final, ya con el elemento de menos
-        System.out.print("Después de la eliminacion al inicio, el array es: ");
+        System.out.print("Después de la eliminacion selectiva, el array es: ");
         for (int indice = 0; indice < largo; indice++) {
-            System.out.print(eliminacionini.get(indice) + " ");
+            System.out.print(eliminacionselec.get(indice) + " ");
         }
         System.out.println();
     }
