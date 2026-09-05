f625d83562cffee8aa2daa3584b45ffd74284c6c
Alizee <alizeesola.america@gmail.com>
2026-09-05 14:48:17 -0700

    Pego las declaraciones de la busqueda secuencial

---
+++ Java/Busqueda_binaria.java
@@ -0,0 +1,8 @@
+public class Busqueda_binaria {
+    public static void main(String[] args) {
+        //Declaracion de arreglo
+        int[] arreglo = {12, 34, 10, 6, 40, 89, 98, 57, 19, 69};
+        int elemento = 40;
+        int largo = arreglo.length; //variable que guarda el largo del arreglo
+    }
+}
