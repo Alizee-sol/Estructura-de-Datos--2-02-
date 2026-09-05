00fde0f34a31c741abd4abae7591dad7bd30c9e4
Alizee <alizeesola.america@gmail.com>
2026-09-05 14:25:40 -0700

    Uso funcion .remove para eliminar el ultimo elemento y hago los cambios insercion a eliminacion

--- Java/Eliminacion_final.java
+++ Java/Eliminacion_final.java
@@ -5,28 +5,27 @@ public class Eliminacion_final {
     public static void main(String[] args) 
     {
         //Declaracion de array que pueda ser cambiado (dinamico)
-        ArrayList<Integer> insercionfin = new ArrayList<>(Arrays.asList(11, 21, 31, 41, 51, 61));
-        int elemento = 52;
+        ArrayList<Integer> eliminacion = new ArrayList<>(Arrays.asList(11, 21, 31, 41, 51, 61));
 
-        System.out.print("Antes de la insercion al final, el array es: ");
+        System.out.print("Antes de la eliminacion al final, el array es: ");
 
         //variable que guarda el largo del array
-        int largo = insercionfin.size();
+        //para saber cual es el último
+        int largo = eliminacion.size();
 
-        //For para recorrer el array (sin insertar nada)
+        //For para recorrer el array (sin eliminar nada)
         for (int indice = 0; indice < largo; indice++) {
-            System.out.print(insercionfin.get(indice) + " ");
+            System.out.print(eliminacion.get(indice) + " ");
         }
         System.out.println();
         
-        //Función para agregar un elemento
-        //Sin especificar el indice, asume que es al final del array
-        insercionfin.add(elemento);
+        //Función para eleminar un elemento ubicado al ultimo del array
+        eliminacion.remove(largo - 1);
         
-        //Recorre el arreglo final, ya con el elemento nuevo
-        System.out.print("Después de la insercion, el array es: ");
+        //Recorre el arreglo final, ya con el elemento de menos
+        System.out.print("Después de la eliminacion, el array es: ");
         for (int indice = 0; indice < largo; indice++) {
-            System.out.print(insercionfin.get(indice) + " ");
+            System.out.print(eliminacion.get(indice) + " ");
         }
         System.out.println();
     }
