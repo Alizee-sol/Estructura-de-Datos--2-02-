ee9b2d2a4028d026f6953ee4bf0a2ec3c3a72b37
Alizee <alizeesola.america@gmail.com>
2026-09-05 16:25:00 -0700

    Cree la funcion findEle para encontrar el indice del elemento a buscar

--- Java/Busqueda_secuencial.java
+++ Java/Busqueda_secuencial.java
@@ -1,8 +1,20 @@
 public class Busqueda_secuencial {
+
+    static int findEle(int[] arreglo, int largo, int elemento){
+        for(int indice =0; indice < largo; indice++){
+            if(arreglo[indice] == elemento){
+                return indice;
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args) {
         //Declaracion de arreglo
         int[] arreglo = {12, 34, 10, 6, 40, 89, 98, 57, 19, 69};
         int elemento = 40;
         int largo = arreglo.length; //variable que guarda el largo del arreglo
+
+        //Envío el arreglo(contenido), el largo(cuantos elementos) y el elemento a buscar
+        int indice = findEle(arreglo, largo, elemento);
     }
 }
