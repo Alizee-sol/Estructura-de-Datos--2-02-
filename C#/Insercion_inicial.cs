5fb25189f3b66f00a01246f155f409b88a6b134d
Alizee <alizeesola.america@gmail.com>
2026-09-05 16:57:53 -0700

    Utilizo listas para declarar el array al que voy a insertar elementos

---
+++ C#/Insercion_inicial.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+class Program {
+    static void Main() {
+        //uso una lista para poder insertar elementos en el array
+        List<int> insercion = new List<int> { 11, 21, 31, 41, 51, 61 };
+        int ele = 52; 
+        
+    } 
+}
