c3802c9cf292fc7e511e78807f205d179377c225
Alizee <alizeesola.america@gmail.com>
2026-09-05 19:12:11 -0700

    Agrego la funcion de findEle

--- C++/Busqueda_secuencial.cpp
+++ C++/Busqueda_secuencial.cpp
@@ -2,13 +2,24 @@
 
 using namespace std;
 
+//funcion de busqueda
+int findEle(int arreglo[], int s, int elemento) {
+    for (int indice = 0; indice < s; indice++) {
+        if (arreglo[indice] == elemento) {
+            return indice;
+        }
+    }
+    return -1;
+}
+
 int main() {
     int arreglo[] = {12, 34, 10, 6, 40, 89, 98, 57, 19, 69};
     int elemento = 19;
     // Calculamos el tamaño del arreglo
     int s = sizeof(arreglo) / sizeof(arreglo[0]);
     
-    int indice;
+    //Mando el arreglo, el largo (s) y el elemento por buscar a la funcion  
+    int indice = findEle(arreglo, s, elemento);
     
     if (indice != -1) {
         cout << "El elemento se encuentra en la posicion: " << (indice + 1) << endl;
