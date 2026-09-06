732f4f58c38848308dac77c2ec8827176a8fe94a
Alizee <alizeesola.america@gmail.com>
2026-09-05 17:05:48 -0700

    Agrego recorrido y largo de la variable

--- C#/Insercion_inicial.cs
+++ C#/Insercion_inicial.cs
@@ -6,6 +6,14 @@ class Program {
         //uso una lista para poder insertar elementos en el array
         List<int> insercion = new List<int> { 11, 21, 31, 41, 51, 61 };
         int ele = 52; 
-        
+
+        int largo = insercion.Count; //variable que guarda el largo del arreglo
+
+        //Recorro la lista para mostrar los elementos
+        Console.Write("Antes de la insercion, el array es: ");
+        for (int indice = 0; indice < largo; indice++) {
+            Console.Write(insercion[indice] + " ");
+        }
+        Console.WriteLine();
     } 
 }
