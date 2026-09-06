d0c103fb1282cb258a74c1f4844a5a8685e08c50
Alizee <alizeesola.america@gmail.com>
2026-09-05 17:42:08 -0700

    Uso .removeAt para eliminar elemento del indice 0 y los cambios correspondientes

--- C#/Eliminacion_inicial.cs
+++ C#/Eliminacion_inicial.cs
@@ -4,25 +4,25 @@ using System.Collections.Generic;
 class Program {
     static void Main() {
         //uso una lista para poder eliminar elementos del array
-        List<int> eliminacion = new List<int> { 11, 21, 31, 41, 51, 61 };
+        List<int> eliminacionini = new List<int> { 11, 21, 31, 41, 51, 61 };
         int ele = 52; 
 
-        int largo = eliminacion.Count; //variable que guarda el largo del arreglo
+        int largo = eliminacionini.Count; //variable que guarda el largo del arreglo
 
         //Recorro la lista para mostrar el arreglo
-        Console.Write("Antes de la eliminacion, el array es: ");
+        Console.Write("Antes de la eliminacion del elemento en el indice 0, el array es: ");
         for (int indice = 0; indice < largo; indice++) {
-            Console.Write(eliminacion[indice] + " ");
+            Console.Write(eliminacionini[indice] + " ");
         }
         Console.WriteLine();
 
-        //Elimina el elemento del indice penúltimo
-        eliminacion.RemoveAt(largo - 1);
+        //Elimina el elemento del indice 0
+        eliminacionini.RemoveAt(0);
         
         //recorre el arreglo con la eliminacion hecha
-        Console.Write("Después de la eliminacion, el array es: ");
+        Console.Write("Después de la eliminacion del elemento en el indice 0, el array es: ");
         for (int indice = 0; indice < largo; indice++) {
-            Console.Write(eliminacion[indice] + " ");
+            Console.Write(eliminacionini[indice] + " ");
         }
         Console.WriteLine();
     } 
