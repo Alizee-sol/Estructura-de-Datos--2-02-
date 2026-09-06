6946220c007478f8178d0ded665d6ed39a208895
Alizee <alizeesola.america@gmail.com>
2026-09-05 17:59:24 -0700

    Creo la estructura previa a la funcion de busqueda para ambas busquedas

---
+++ C#/Busqueda_secuencial.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program {
+    static void Main() {
+        int[] arreglo = { 12, 34, 10, 6, 40, 89, 98, 57, 19, 69 };
+        int elemento = 40;
+        int largo = arreglo.Length;
+        int indice;
+
+        if (indice != -1) {
+            Console.WriteLine("El elemento se encuentra en la posicion: " + indice);
+        } else {
+            Console.WriteLine("No se encuentra el elemento");
+        }
+    }
+}
