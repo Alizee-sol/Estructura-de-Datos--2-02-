ef2605d04918a8d747bd48cc583dbc16a5903b3d
Alizee <alizeesola.america@gmail.com>
2026-09-05 17:14:53 -0700

    pego codigo de la insercion final para reutilizar estructura

---
+++ C#/Eliminacion_final.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+class Program {
+    static void Main() {
+        //uso una lista para poder insertar elementos en el array
+        List<int> insercionfin = new List<int> { 11, 21, 31, 41, 51, 61 };
+        int ele = 52; 
+
+        int largo = insercionfin.Count; //variable que guarda el largo del arreglo
+
+        //Recorro la lista para mostrar los elementos
+        Console.Write("Antes de la insercion, el array es: ");
+        for (int indice = 0; indice < largo; indice++) {
+            Console.Write(insercionfin[indice] + " ");
+        }
+        Console.WriteLine();
+
+        //inserta el elemento en el índice 0
+        insercionfin.Insert(largo -1, ele);
+        
+        //recorre el arreglo con la insercionfin hecha
+        Console.Write("Después de la inserción, el array es: ");
+        for (int indice = 0; indice < largo; indice++) {
+            Console.Write(insercionfin[indice] + " ");
+        }
+        Console.WriteLine();
+    } 
+}
