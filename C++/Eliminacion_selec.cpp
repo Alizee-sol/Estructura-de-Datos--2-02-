e53f3e0f8bef9509180cd2f0120814672bdc8aa9
Alizee <alizeesola.america@gmail.com>
2026-09-05 19:01:10 -0700

    Utilizo .erase para eliminar el elemento en sus parametros

--- C++/Eliminacion_selec.cpp
+++ C++/Eliminacion_selec.cpp
@@ -7,6 +7,7 @@ int main() {
     //uso vector para poder insertar elementos a un arreglo
     vector<int> eliminaciones = {11, 21, 31, 41, 51, 61};
     int largo = eliminaciones.size();
+    int idx = 3; //indice del elemento que se va a eliminar
 
     //recorre el array antes de eliminar nada
     cout << "Antes de la eliminacion, el array es: ";
@@ -15,10 +16,10 @@ int main() {
     }
     cout << endl;
     
-    //elimino el elemento al final del array
-    eliminaciones.pop_back();
+    //elimino el elemento contando las posiciones desde el inicio del arreglo
+    eliminaciones.erase(eliminaciones.begin() + (idx - 1));
     
-    cout << "Después de la insersion en el final del array es: ";
+    cout << "Después de la eliminacion selectiva del array es: ";
     for (int indice = 0; indice < largo; indice++) {
         cout << eliminaciones[indice] << " ";
     }
