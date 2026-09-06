f96822192f87b36b629240989933b74c7419d64e
Alizee <alizeesola.america@gmail.com>
2026-09-05 18:07:48 -0700

    Agregué funcion findEle

--- C#/Busqueda_binaria.cs
+++ C#/Busqueda_binaria.cs
@@ -1,11 +1,28 @@
 using System;
 
 class Program {
+    static int FindEle(int[] arreglo, int izquierda, int derecha, int elemento) {
+        while (izquierda <= derecha) {
+            int medio = izquierda + (derecha - izquierda) / 2;
+            
+            if (arreglo[medio] == elemento) {
+                return medio;
+            } else if (arreglo[medio] < elemento) {
+                izquierda = medio + 1;
+            } else {
+                derecha = medio - 1;
+            }
+        }
+        return -1;
+    }
     static void Main() {
         int[] arreglo = { 12, 34, 10, 6, 40, 89, 98, 57, 19, 69 };
         int elemento = 40;
         int largo = arreglo.Length;
-        int indice;
+
+        Array.Sort(arreglo);
+
+        int indice = FindEle(arreglo, 0, largo - 1, elemento);
         
         if (indice != -1) {
             Console.WriteLine("El elemento se encuentra en la posicion: " + indice);
