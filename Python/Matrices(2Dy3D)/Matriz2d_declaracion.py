b3a277403b2a5e42b76b196d8abaecfee0cde1d8
Alizee <alizeesola.america@gmail.com>
2026-09-06 14:54:02 -0700

    Recorrido del arreglo

--- Python/Matrices(2Dy3D)/Matriz2d_declaracion.py
+++ Python/Matrices(2Dy3D)/Matriz2d_declaracion.py
@@ -1,6 +1,13 @@
-#Matriz 2d
+# Matriz 2d //////////////////////////////
 # 
 # Declacion de una matriz 2d
 Array2d = [[1,2,3],
            [4,5,6],
            [7,8,9]]
+
+# Recorrido de la matriz 2d
+print("Los elementos del array son: ")
+for row in Array2d:                   # Recorre renglones
+    for element in row:               # Recorre los elementos de cada renglon
+        print(element, end=" ")
+    print()
