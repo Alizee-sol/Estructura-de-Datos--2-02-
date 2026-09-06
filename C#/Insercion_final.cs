6ddbde727357866126a4a6e631b4ecc6b4f468d7
Alizee <alizeesola.america@gmail.com>
2026-09-05 17:12:42 -0700

    Utilizo .insert pero cambinado el parámetro de 0 a Largo - 1, y hago los cambios correspondientes

--- C#/Insercion_final.cs
+++ C#/Insercion_final.cs
@@ -4,25 +4,25 @@ using System.Collections.Generic;
 class Program {
     static void Main() {
         //uso una lista para poder insertar elementos en el array
-        List<int> insercion = new List<int> { 11, 21, 31, 41, 51, 61 };
+        List<int> insercionfin = new List<int> { 11, 21, 31, 41, 51, 61 };
         int ele = 52; 
 
-        int largo = insercion.Count; //variable que guarda el largo del arreglo
+        int largo = insercionfin.Count; //variable que guarda el largo del arreglo
 
         //Recorro la lista para mostrar los elementos
         Console.Write("Antes de la insercion, el array es: ");
         for (int indice = 0; indice < largo; indice++) {
-            Console.Write(insercion[indice] + " ");
+            Console.Write(insercionfin[indice] + " ");
         }
         Console.WriteLine();
 
         //inserta el elemento en el índice 0
-        insercion.Insert(0, ele);
+        insercionfin.Insert(largo -1, ele);
         
-        //recorre el arreglo con la insercion hecha
-        Console.Write("Después de la eliminacion, el array es: ");
+        //recorre el arreglo con la insercionfin hecha
+        Console.Write("Después de la inserción, el array es: ");
         for (int indice = 0; indice < largo; indice++) {
-            Console.Write(insercion[indice] + " ");
+            Console.Write(insercionfin[indice] + " ");
         }
         Console.WriteLine();
     } 
