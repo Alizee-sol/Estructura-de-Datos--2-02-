27ac39bc6419233f9d16c4633c76964d4b20ee52
Alizee <alizeesola.america@gmail.com>
2026-09-07 09:38:16 -0700

    Uso indice y largo para recorrer desde el finla hasta el principio

--- JavaScript/Recorrido_inverso.js
+++ JavaScript/Recorrido_inverso.js
@@ -1,11 +1,12 @@
 // Declaracion de array
-const recorrido = [40, 50, 60, 70, 80, 90];
+const recorridoin = [40, 50, 60, 70, 80, 90];
+const largo = recorridoin.length;
 
 console.log("Recorrido lineal (secuencial) es: ");
 
 //For(of) para recorrer arreglo
-for (const elemento of recorrido) {  //Recorre cada elemento del array
-    console.log( elemento + " ");;
+for (let indice = largo - 1; indice >= 0; indice--) {
+    console.log(recorridoin[indice] + " ");
 }
 
 console.log();
