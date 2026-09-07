3cd2fcf39e05685c9981eb7d401270b5f3262868
Alizee <alizeesola.america@gmail.com>
2026-09-07 09:45:50 -0700

    Adaptacion del codigo

--- JavaScript/Insercion_inicial.js
+++ JavaScript/Insercion_inicial.js
@@ -1,14 +1,14 @@
-// Declaracion de array
-const recorridoin = [40, 50, 60, 70, 80, 90];
+// Declaracion de array y el elemento que vamos a insertar
+const insercion = [11, 21, 31, 41, 51, 61];
+const ele = 52;
 
 // Variable que va a guardar el largo del array
-const largo = recorridoin.length;
+const largo = insercion.length;
 
-console.log("Recorrido lineal (secuencial) es: ");
-
-//For(of) para recorrer arreglo desde el final al principio
-for (let indice = largo - 1; indice >= 0; indice--) {
-    console.log(recorridoin[indice] + " ");
+//Recorrer el antes
+console.log("El arreglo antes de la insercion es: ");
+for (let indice = 0; indice < largo; indice++) {
+    console.log(insercion[indice] + " ");
 }
 
 console.log();
