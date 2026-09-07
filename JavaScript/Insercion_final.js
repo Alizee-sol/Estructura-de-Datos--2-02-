7dbf30610ec8fc259c5bc61329ef9a797793d6c9
Alizee <alizeesola.america@gmail.com>
2026-09-07 09:53:01 -0700

    Adaptacion del codigo y uso .push para insertar al final

--- JavaScript/Insercion_final.js
+++ JavaScript/Insercion_final.js
@@ -1,22 +1,22 @@
 // Declaracion de array y el elemento que vamos a insertar
-const insercion = [11, 21, 31, 41, 51, 61];
+const insercionfin = [11, 21, 31, 41, 51, 61];
 const ele = 52;
 
 // Variable que va a guardar el largo del array
-const largo = insercion.length;
+const largo = insercionfin.length;
 
 //Recorrer el antes
-console.log("El arreglo antes de la insercion es: ");
+console.log("El arreglo antes de la insercion final es: ");
 for (let indice = 0; indice < largo; indice++) {
-    console.log(insercion[indice] + " ");
+    console.log(insercionfin[indice] + " ");
 }
- // Empujo el ele al indice 0 y recorre el resto
-insercion.unshift(ele);
+ // Empujo el ele al final del arreglo
+insercionfin.push(ele);
 
 // recorrido del resultado
-console.log("El arreglo antes de la insercion es: ");
+console.log("El arreglo antes de la insercion final es: ");
 for (let indice = 0; indice < largo; indice++) {
-    console.log(insercion[indice] + " ");
+    console.log(insercionfin[indice] + " ");
 }
 
 console.log();
