30152b24d372bed76c0ae5fb8347988e00008e90
Alizee <alizeesola.america@gmail.com>
2026-09-07 10:03:05 -0700

    Adapto codigo y uso .shift para eliminar el primer elemento

--- JavaScript/Eliminacion_inicial.js
+++ JavaScript/Eliminacion_inicial.js
@@ -1,21 +1,21 @@
 // Declaracion de array
-const eliminacionfin = [11, 21, 31, 41, 51, 61];
+const eliminacion = [11, 21, 31, 41, 51, 61];
 
 // Variable que va a guardar el largo del array
-const largo = eliminacionfin.length;
+const largo = eliminacion.length;
 
 //Recorrer el antes
-console.log("El arreglo antes de la eliminacion final es: ");
+console.log("El arreglo antes de la eliminacion inicial es: ");
 for (let indice = 0; indice < largo; indice++) {
-    console.log(eliminacionfin[indice] + " ");
+    console.log(eliminacion[indice] + " ");
 }
-// elimina el ultimo elemento del array
-eliminacionfin.pop();
+// elimina el primer elemento del array
+eliminacion.shift();
 
 // recorrido del resultado
-console.log("El arreglo antes de la eliminacion final es: ");
+console.log("El arreglo antes de la eliminacion inicial es: ");
 for (let indice = 0; indice < largo; indice++) {
-    console.log(eliminacionfin[indice] + " ");
+    console.log(eliminacion[indice] + " ");
 }
 
 console.log();
