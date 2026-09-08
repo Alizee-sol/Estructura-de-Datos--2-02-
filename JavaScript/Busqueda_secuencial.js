95c1674d51b03a2bf886ea13e76ea3801c2990a2
Alizee <alizeesola.america@gmail.com>
2026-09-07 17:18:58 -0700

    Reutilizo pedazo de busqueda binaria

---
+++ JavaScript/Busqueda_secuencial.js
@@ -0,0 +1,9 @@
+const arreglo = [12, 34, 10, 6, 40, 89, 98, 57, 19, 69];
+const elemento = 19;
+const largo = arreglo.length;
+
+if (indice !== -1) {
+    console.log("El elemento se encuentra en la posicion: " + (indice + 1));
+} else {
+    console.log("No se encuentra el elemento");
+}
