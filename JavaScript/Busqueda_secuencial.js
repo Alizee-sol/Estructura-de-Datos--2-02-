5ebb7f56190c788470279cfe6f2e582faabede6f
Alizee <alizeesola.america@gmail.com>
2026-09-07 17:24:12 -0700

    Funcion de busqueda secuencial

--- JavaScript/Busqueda_secuencial.js
+++ JavaScript/Busqueda_secuencial.js
@@ -1,6 +1,16 @@
+function findEle(arreglo, largo, elemento) {
+    for (let indice = 0; indice < largo; indice++) {
+        if (arreglo[indice] === elemento) {
+            return indice;
+        }
+    }
+    return -1;
+}
 const arreglo = [12, 34, 10, 6, 40, 89, 98, 57, 19, 69];
 const elemento = 19;
 const largo = arreglo.length;
+
+const indice = findEle(arreglo, largo, elemento);
 
 if (indice !== -1) {
     console.log("El elemento se encuentra en la posicion: " + (indice + 1));
