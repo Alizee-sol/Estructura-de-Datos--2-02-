97bd6756e97b54f3ffc5215379f667f6339809b5
Alizee <alizeesola.america@gmail.com>
2026-09-07 17:30:44 -0700

    Busqueda binaria usando .sort y math.floor

--- JavaScript/Busqueda_binaria.js
+++ JavaScript/Busqueda_binaria.js
@@ -1,6 +1,27 @@
+function findEle(arreglo, izquierda, derecha, elemento) {
+    while (izquierda <= derecha) {
+        // Math.floor = '//' de Python
+        let medio = izquierda + Math.floor((derecha - izquierda) / 2);
+        
+        if (arreglo[medio] === elemento) {
+            return medio;
+        } else if (arreglo[medio] < elemento) {
+            izquierda = medio + 1;
+        } else {
+            derecha = medio - 1;
+        }
+    }
+    return -1;
+}
+
 const arreglo = [12, 34, 10, 6, 40, 89, 98, 57, 19, 69];
-const elemento = 19;
+const elemento = 40;
 const largo = arreglo.length;
+
+//(a, b) = elementos a buscar
+//(a - b) = revisa que a - b = positivo
+arreglo.sort((a, b) => a - b);
+const indice = findEle(arreglo, 0, largo - 1, elemento);
 
 if (indice !== -1) {
     console.log("El elemento se encuentra en la posicion: " + (indice + 1));
