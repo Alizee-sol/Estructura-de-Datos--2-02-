39cadfee85555df6bb0478c82e2c91a503ee1893
Alizee <alizeesola.america@gmail.com>
2026-09-07 17:08:57 -0700

    Adapto el codigo para eliminar selectivo

--- JavaScript/Eliminacion_selec.js
+++ JavaScript/Eliminacion_selec.js
@@ -1,21 +1,19 @@
 // Declaracion de array
-const eliminacionfin = [11, 21, 31, 41, 51, 61];
+const eliminacionse = [11, 21, 31, 41, 51, 61];
 
 // Variable que va a guardar el largo del array
-const largo = eliminacionfin.length;
+const largo = eliminacionse.length;
 
 //Recorrer el antes
-console.log("El arreglo antes de la eliminacion final es: ");
+console.log("El arreglo antes de la eliminacion selectiva es: ");
 for (let indice = 0; indice < largo; indice++) {
-    console.log(eliminacionfin[indice] + " ");
+    console.log(eliminacionse[indice] + " ");
 }
-// elimina el ultimo elemento del array
-eliminacionfin.pop();
 
 // recorrido del resultado
-console.log("El arreglo antes de la eliminacion final es: ");
+console.log("El arreglo antes de la eliminacion selectiva es: ");
 for (let indice = 0; indice < largo; indice++) {
-    console.log(eliminacionfin[indice] + " ");
+    console.log(eliminacionse[indice] + " ");
 }
 
 console.log();
