dfe32dd9ebd852e4e476199d6b591854d57dda73
Alizee <alizeesola.america@gmail.com>
2026-09-07 17:12:01 -0700

    Uso .splice para eliminar el indice seleccionado

--- JavaScript/Eliminacion_selec.js
+++ JavaScript/Eliminacion_selec.js
@@ -3,12 +3,17 @@ const eliminacionse = [11, 21, 31, 41, 51, 61];
 
 // Variable que va a guardar el largo del array
 const largo = eliminacionse.length;
+// variables que va a guardar el indice a eliminar
+const idx = 3;
 
 //Recorrer el antes
 console.log("El arreglo antes de la eliminacion selectiva es: ");
 for (let indice = 0; indice < largo; indice++) {
     console.log(eliminacionse[indice] + " ");
 }
+
+// Elimina. (Lndice, número a eliminar)
+eliminacionse.splice(posicion - 1, 1);
 
 // recorrido del resultado
 console.log("El arreglo antes de la eliminacion selectiva es: ");
