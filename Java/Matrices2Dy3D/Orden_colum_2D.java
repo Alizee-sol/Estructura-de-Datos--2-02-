10ca20b30e3c2bef147fe7babfcd4fdc83c43941
Alizee <alizeesola.america@gmail.com>
2026-09-08 22:26:10 -0700

    Reutilicé codigo de orden_filas y cambie posiciones de x y Y

---
+++ Java/Matrices2Dy3D/Orden_colum_2D.java
@@ -0,0 +1,48 @@
+package Matrices2Dy3D;
+
+public class Orden_colum_2D {
+    
+    public static void main(String[] args) 
+    {
+        
+        //Declaracion del tamaño de X y Y
+        int xs = 3, ys = 3;
+        
+        //Declaracion del arreglo vacío donde se van a acomodar los elementos del arreglo 2d
+        int[] arr = new int[xs * ys];
+
+        int[][] Array2d = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Los elementos del array 2D son: ");
+        for (int[] row : Array2d)               //Recorre renglones
+        {
+            for (int element : row)             //Recorre los elementos de cada renglon
+                {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+
+        int k = 0;
+        for (int y = 0; y < ys; y++) 
+        {
+            for (int x = 0; x < xs; x++) 
+            {
+                k = x * xs + y;
+                arr[k] = Array2d[x][y];
+                k = k + 1;
+            }
+        }
+
+        System.out.println("Los elementos del array 3D son: ");
+        for (int y = 0; y < ys; y++) {
+            for (int x = 0; x < xs; x++) {
+                System.out.print(arr[x * xs + y] + " ");
+            }
+        }
+    }
+}
