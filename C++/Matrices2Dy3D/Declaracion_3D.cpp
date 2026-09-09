97fc15e1884e064d858cbcad8edd4a8ed8641078
Alizee <alizeesola.america@gmail.com>
2026-09-09 12:17:33 -0700

    Modifiqué el arreglo de 2d a 3d y añadi un for

--- C++/Matrices2Dy3D/Declaracion_3D.cpp
+++ C++/Matrices2Dy3D/Declaracion_3D.cpp
@@ -5,21 +5,26 @@ using namespace std;
 
 int main() {
 
-    //Declaracion de array2d (3*3)
-    int Array2d[3][3] = {
+    //Declaracion de array3d (2)pisos de (3)x y (3)y
+    int Array3d[2][3][3] = {{
         {1, 2, 3},
         {4, 5, 6},
         {7, 8, 9}
-    };
+    }, {
+        {11, 12, 13},
+        {14, 15, 16},
+        {17, 18, 19}
+    }};
 
     cout << "Los elementos del array son: ";
     cout << endl;
     
-    //auto% detecta en que renglon se encuentra
-    for (auto& row : Array2d) {          //Recorre renglones
-        for (int element : row) {        //Recorre los elementos de cada renglon
-            cout << element << " ";
+    for (auto& Array2d : Array3d) {
+        for (auto& row : Array2d) {          //Recorre renglones
+            for (int element : row) {        //Recorre los elementos de cada renglon
+                cout << element << " ";
+            }
+            cout << endl;
         }
-        cout << endl;
     }
 }
