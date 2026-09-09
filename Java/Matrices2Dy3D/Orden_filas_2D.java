d26b0550bd71293e2a440ebb848647487b3e9380
Alizee <alizeesola.america@gmail.com>
2026-09-08 21:24:18 -0700

    Reutilizo codigo de Declaracion 2d

---
+++ Java/Matrices2Dy3D/Orden_filas_2D.java
@@ -0,0 +1,22 @@
+package Matrices2Dy3D;
+
+public class Orden_filas_2D {
+    public static void main(String[] args) {
+        int[][] Array2d = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Los elementos del array son: ");
+        
+        for (int[] row : Array2d)               //Recorre renglones
+            {
+            for (int element : row)             //Recorre los elementos de cada renglon
+                {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+    }
+}
