fbfe7408ee92bc4c634407db4a12cfc01800ed8f
Alizee <alizeesola.america@gmail.com>
2026-09-08 21:22:57 -0700

    Declaracion de Matriz 2d basada en python

--- Python/Algoritmos_Orden/Bubble.py
+++ Python/Algoritmos_Orden/Bubble.py
@@ -1,8 +1,10 @@
-bubbleSort(array)
-n = length(array)
+def bubbleSort(array):
+    s = len(array)
 
-repeat
-    swapped = False
-    for i = 1 to n-1
-        if array[i-1] > array[i], then
-        swap(array[i-1], array[i])
+    for j in range(s)
+    
+        swapped = False
+
+        for i = 1 to n-1
+            if array[i-1] > array[i], then
+            swap(array[i-1], array[i])
