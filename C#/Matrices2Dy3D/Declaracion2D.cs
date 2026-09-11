ed8af3d1b9141e805a5a7e61a0ce4f41905d8147
Alizee <alizeesola.america@gmail.com>
2026-09-10 21:02:59 -0700

    Recorrido de Array2d

--- C#/Matrices2Dy3D/Declaracion2D.cs
+++ C#/Matrices2Dy3D/Declaracion2D.cs
@@ -1,13 +1,28 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
-    //Declaracion de array2d
-    int[][] Array2d = {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9}
-    };
+    static void Main()
+    {
+        //Declaracion de array2d
+        int[][] Array2d = new int[][] 
+        {
+            new int[] {1, 2, 3},
+            new int[] {4, 5, 6},
+            new int[] {7, 8, 9}
+        };
 
-    
+        Console.WriteLine("Los elementos de array son: ");
+        foreach(int[] row in array2d)
+        {
+            foreach(int elemento in row)
+            {
+                Console.Write(elemento + " ");
+            }
+            Console.WriteLine();
+        }    
+    }
+
+
 }
