227100080d31c24e5e8bdaf2d56c92088500eebd
Alizee <alizeesola.america@gmail.com>
2026-09-11 06:54:24 -0700

    Void insercion

--- C++/Algoritmos_Orden/Insercion.cpp
+++ C++/Algoritmos_Orden/Insercion.cpp
@@ -2,6 +2,22 @@
 
 using namespace std;
 
+void insercion(int arreglo[], int largo)
+{
+    for (int indice = 1; indice < largo; indice++)         //Recorre arreglo
+    {
+        int elegido = arreglo[indice];                     //Asigna al indice actual como elegido
+        int puntero = indice - 1;                          //Asigna puntero al siguiente
+        
+        while (puntero >= 0 && elegido < arreglo[puntero]) //revisa que el numero actual sea mas grande que el elegido
+        {
+            arreglo[puntero + 1] = arreglo[puntero];
+            puntero = puntero - 1;                         //Compara el siguiente
+        }
+        arreglo[puntero + 1] = elegido;                    //Encontró uno mas pequeño y es el nuevo temporal
+    }
+}
+
 void recorrido(int arreglo[], int largo)
 {
     for (int indice = 0; indice < largo; indice++)
@@ -17,6 +33,8 @@ int main()
 
     cout << "Antes de ordenar los elementos del array son: ";
     recorrido(arreglo, largo);
+
+    insercion(arreglo, largo);
 
     cout << "Después de ordenar los elementos del array son: ";
     recorrido(arreglo, largo);
