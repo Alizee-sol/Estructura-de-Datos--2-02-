5f97503fc566e47b78034351e1db2945c6136a97
Alizee <alizeesola.america@gmail.com>
2026-09-10 21:09:54 -0700

    Formula para vaciar 2d en 1d (x * xs + y)

--- C#/Matrices2Dy3D/Orden_filas2D.cs
+++ C#/Matrices2Dy3D/Orden_filas2D.cs
@@ -2,6 +2,9 @@ using System;
 
 class Program {
     static void Main() {
+
+        int xs = 3, ys = 3;
+        int[] arr = new int[xs * ys];
         
         int[][] Array2d = new int[][] 
         {
@@ -9,6 +12,18 @@ class Program {
             new int[] { 4, 5, 6 },
             new int[] { 7, 8, 9 }
         };
+
+        //Formula para vaciar array2d en fila 1d
+        int k = 0;
+        for (int x = 0; x < xs; x++) 
+        {
+            for (int y = 0; y < ys; y++) 
+            {
+                k = x * xs + y;
+                arr[k] = Array2d[x][y];
+                k = k + 1;
+            }
+        }
 
         Console.WriteLine("Los elementos del array son: ");
         foreach (int[] row in Array2d) 
@@ -19,5 +34,16 @@ class Program {
             }
             Console.WriteLine();
         }
+
+        //Recorrido con resultado
+        Console.WriteLine("Los elementos del array unidimensional son: ");
+        for (int x = 0; x < xs; x++) 
+        {
+            for (int y = 0; y < ys; y++) 
+            {
+                Console.Write(arr[x * xs + y] + " ");
+            }
+        }
+        Console.WriteLine();
     }
 }
