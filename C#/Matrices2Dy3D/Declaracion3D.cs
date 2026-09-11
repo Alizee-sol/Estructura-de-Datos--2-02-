6663e33dd55ba379fc920e08b19287414463aa3a
Alizee <alizeesola.america@gmail.com>
2026-09-10 21:23:52 -0700

    Cambios correspondientes a recorrido 3d

--- C#/Matrices2Dy3D/Declaracion3D.cs
+++ C#/Matrices2Dy3D/Declaracion3D.cs
@@ -3,19 +3,30 @@ using System;
 class Program {
     static void Main() {
         
-        int[][] Array2d = new int[][] 
+        int[][][] Array3d = new int[][][] 
         {
+        new int[][] {
             new int[] { 1, 2, 3 },
             new int[] { 4, 5, 6 },
             new int[] { 7, 8, 9 }
-        };
+        },
+        new int[][] 
+        {
+            new int[] { 11, 12, 13 },
+            new int[] { 14, 15, 16 },
+            new int[] { 17, 18, 19 }
+        }};
 
         Console.WriteLine("Los elementos del array son: ");
-        foreach (int[] row in Array2d) 
+        foreach (int[][] Array2d in Array3d) 
         {
-            foreach (int element in row) 
+            foreach (int[] row in Array2d) 
             {
-                Console.Write(element + " ");
+                Console.WriteLine(); 
+                foreach (int element in row) 
+                {
+                    Console.Write(element + " ");
+                }
             }
             Console.WriteLine();
         }
