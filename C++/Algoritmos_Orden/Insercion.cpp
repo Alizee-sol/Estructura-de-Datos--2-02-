1e401e24904b5d225b4ab16281677232c9b1fb92
Alizee <alizeesola.america@gmail.com>
2026-09-11 06:43:35 -0700

    Void de recorrido

--- C++/Algoritmos_Orden/Insercion.cpp
+++ C++/Algoritmos_Orden/Insercion.cpp
@@ -2,20 +2,24 @@
 
 using namespace std;
 
+void recorrido(int arreglo[], int largo)
+{
+    for (int indice = 0; indice < largo; indice++)
+    {
+        cout << arreglo[indice] << " ";
+    }
+}
+
 int main()
 {
     int arreglo[] = {15,16,11,13,14};
     int largo = sizeof(arreglo) / sizeof(arreglo[0]);
 
     cout << "Antes de ordenar los elementos del array son: ";
-    for (int indice = 0; indice < largo; indice++)
-    {
-        cout << arreglo[indice] << " ";
-    }
+    recorrido(arreglo, largo);
 
     cout << "Después de ordenar los elementos del array son: ";
-    for (int indice = 0; indice < largo; indice++) {
-        cout << arreglo[indice] << " ";
-    }
+    recorrido(arreglo, largo);
+
     return 0;
 }
