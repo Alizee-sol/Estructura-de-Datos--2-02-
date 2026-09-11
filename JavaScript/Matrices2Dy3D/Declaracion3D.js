be25914fc85ac0056611eaa05d4b83edb46165cb
Alizee <alizeesola.america@gmail.com>
2026-09-10 21:50:03 -0700

    Hice los cambios correspondientes

--- JavaScript/Matrices2Dy3D/Declaracion3D.js
+++ JavaScript/Matrices2Dy3D/Declaracion3D.js
@@ -1,12 +1,17 @@
-const Array2d = [
+const Array3d = [[
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
-];
+], [
+    [11, 12, 13],
+    [14, 15, 16],
+    [17, 18, 19]
+]];
 
 console.log("Los elementos del array son: ");
 
-// Recorre renglones
+for (const Array2d of Array3d)
+{
 for (const row of Array2d) {
     console.log();
     
@@ -14,6 +19,6 @@ for (const row of Array2d) {
         console.log(element + " ");
     }
     
-    // Imprime la fila completa y hace el salto automático
     console.log();
+}
 }
