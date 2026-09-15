965680d137d58603104257d8fb4ab4f3e4fa0bd9
Alizee <alizeesola.america@gmail.com>
2026-09-15 09:11:57 -0700

    Funciones Print y Seleccion

--- C#/Algoritmos_Orden/Selectiva.cs
+++ C#/Algoritmos_Orden/Selectiva.cs
@@ -2,6 +2,32 @@ using System;
 
 class Program
 {
+    public void seleccion(int[] arreglo, int largo)
+    {
+        for(int i = 0; i < largo; i++)
+        {
+            //elemento pequeño
+            int small = i;
+            for(int j = i + 1; j < largo; j++)
+            {
+                if (arreglo[small] > arreglo[j])
+                {
+                    small = j;
+                }
+            }
+            int temp = arreglo[i];        //vacía i 
+            arreglo[i] = arreglo[small];  //le da a i el valor de small
+            arreglo[small] = temp;        //le da a small el valor de i
+        }
+    }
+
+    public void printArr(int[] arreglo, int largo)
+    {
+        for(int i = 0; i < largo; i++)
+        {
+            Console.WriteLine(arreglo[i] + " ");
+        }
+    }
     static void main()
     {
         int[] arr = {65, 26, 13, 23, 12};
