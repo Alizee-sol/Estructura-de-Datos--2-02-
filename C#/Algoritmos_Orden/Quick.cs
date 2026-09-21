d78a4a1be12d44fdbe6178cec3e45ac0746c4b38
Alizee <alizeesola.america@gmail.com>
2026-09-21 09:59:26 -0700

    Void QckSort enlazada con Paticion

--- C#/Algoritmos_Orden/Quick.cs
+++ C#/Algoritmos_Orden/Quick.cs
@@ -4,6 +4,20 @@ class program
 {
     static void main()
     {
+
+        static int Partition(int[] a, int l, int h) 
+        {
+        
+        }
+        static void QckSort(int[] a, int l, int h) 
+        {
+            if (l < h) {
+                int pi = Partition(a, l, h);
+                QckSort(a, l, pi - 1);
+                QckSort(a, pi + 1, h);
+            }
+        }
+
         int[] arreglo = { 10, 7, 8, 9, 1, 5 };
         int largo = arreglo.Length;
         
@@ -13,6 +27,8 @@ class program
         }
         Console.WriteLine();
         
+        QckSort(a, 0, size - 1);
+
         Console.WriteLine("El arreglo después de ordenarlo: ");
         foreach (int i in largo) {
             Console.Write(i + " ");
