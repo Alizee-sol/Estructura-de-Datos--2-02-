097f2ae42897f51ef5ffb018cc74c1b162dd3176
Alizee <alizeesola.america@gmail.com>
2026-09-21 21:17:31 -0700

    Funcion qcksort sin particion ni swap

--- C++/Algoritmos_Orden/Quick.cpp
+++ C++/Algoritmos_Orden/Quick.cpp
@@ -2,6 +2,22 @@
 
 using namespace std;
 
+void Particion(int[] a, int l, int h)
+{
+    int pvt = a[h];
+    int j = l - 1;
+}
+
+void QckSort(int a[], int l, int h)
+{
+    if (l < h)
+        {
+            int pi = Particion(a, l, h);
+            QckSort(a, l, pi - 1);
+            QckSort(a, pi + 1, h);
+        }
+}
+
 int main()
 {
     int arreglo[] = {10,7,8,9,1,5};
@@ -12,6 +28,8 @@ int main()
     {
         cout << arreglo[indice] << " ";
     }
+
+    QckSort(arreglo, 0, largo - 1);
 
     cout << "\nEl arreglo despues de ordenar es: " << endl;
     for (int indice = 0; indice < largo; indice++)
