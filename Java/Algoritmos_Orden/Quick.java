6d88d9e15ae22256aa48e6ebb0d62af43a0ff2bb
Alizee <alizeesola.america@gmail.com>
2026-09-22 22:14:30 -0700

    Funciones QckSort, Particion y swap

--- Java/Algoritmos_Orden/Quick.java
+++ Java/Algoritmos_Orden/Quick.java
@@ -2,9 +2,33 @@ package Algoritmos_Orden;
 
 public class Quick 
 {
-    public static void QckSort(int[] arreglo, int l, int largo)
+    static void swap(int[] arreglo, int j, int largo) 
     {
-
+        int temp = arreglo[j];
+        arreglo[j] = arreglo[largo];
+        arreglo[largo] = temp;
+    }
+    static int particion(int[] arreglo, int l, int largo) 
+    {
+        int pvt = arreglo[largo];
+        int j = l - 1;
+        for (int k = l; k < largo; k++) {
+            if (arreglo[k] < pvt) {
+                j++;
+                swap(arreglo, j, k);
+            }
+        }
+        swap(arreglo, j + 1, largo);
+        return j + 1;
+    }
+    static void QckSort(int[] arreglo, int l, int largo)
+    {
+        if (l < largo)
+        {
+            int pi = particion(arreglo, l, largo);
+            QckSort(arreglo, l, pi - 1);
+            QckSort(arreglo, pi + 1, largo);
+        }
     }
     public static void main(String[] args) {
         int[] arreglo = {10, 7, 8, 9, 1, 5};
