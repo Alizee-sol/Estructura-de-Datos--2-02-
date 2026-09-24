4424f8ee41a4e59086c750f5501895af31a701b2
Alizee <alizeesola.america@gmail.com>
2026-09-23 21:02:10 -0700

    Recorridos sin fuinciones

---
+++ JavaScript/Algoritmos_Orden/Quick.js
@@ -0,0 +1,14 @@
+let arreglo = [10, 7, 8, 9, 1, 5];
+let largo = arreglo.length;
+
+console.log("Antes de ordenar los elementos del array son: ");
+for (const j of arreglo) {
+    console.log(j + " ");
+}
+console.log();
+
+console.log("Después de ordenar los elementos del array son: ");
+for (let j = 0; j < largo; j++) {
+    console.log(arreglo[j] + " ");
+}
+console.log();
