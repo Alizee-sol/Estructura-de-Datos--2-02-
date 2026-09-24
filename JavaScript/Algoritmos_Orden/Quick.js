c6d8fb88fdab3c61f56f9bf3485e390abfac1703
Alizee <alizeesola.america@gmail.com>
2026-09-23 21:03:52 -0700

    Funcion qcksort

--- JavaScript/Algoritmos_Orden/Quick.js
+++ JavaScript/Algoritmos_Orden/Quick.js
@@ -1,3 +1,22 @@
+function swap(a, j, k) 
+{
+
+}
+
+function partition(a, l, h) 
+{
+
+}
+
+function qckSort(a, l, h) 
+{
+    if (l < h) {
+        let pi = partition(a, l, h);
+        qckSort(a, l, pi - 1);
+        qckSort(a, pi + 1, h);
+    }
+}
+
 let arreglo = [10, 7, 8, 9, 1, 5];
 let largo = arreglo.length;
 
@@ -6,6 +25,8 @@ for (const j of arreglo) {
     console.log(j + " ");
 }
 console.log();
+
+qckSort(arreglo, 0, largo - 1)
 
 console.log("Después de ordenar los elementos del array son: ");
 for (let j = 0; j < largo; j++) {
