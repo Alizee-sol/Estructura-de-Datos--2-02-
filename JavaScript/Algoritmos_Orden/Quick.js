3535fe9dd89f4f3764d8c72a7cd1fb96e9538c40
Alizee <alizeesola.america@gmail.com>
2026-09-23 21:08:33 -0700

    Funciones Swap y partition

--- JavaScript/Algoritmos_Orden/Quick.js
+++ JavaScript/Algoritmos_Orden/Quick.js
@@ -1,11 +1,22 @@
 function swap(a, j, k) 
 {
-
+    let temp = a[j];
+    a[j] = a[k];
+    a[k] = temp;
 }
 
 function partition(a, l, h) 
 {
-
+    let pvt = a[h];
+    let j = l - 1;
+    for (let k = l; k < h; k++) {
+        if (a[k] < pvt) {
+            j++;
+            swap(a, j, k);
+        }
+    }
+    swap(a, j + 1, h);
+    return j + 1;
 }
 
 function qckSort(a, l, h) 
